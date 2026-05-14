093656d233a0966ecb6ac64849d395026b3091d8
LeCasiNoze <kasinoze@gmail.com>
2026-05-14 17:35:45 +0200

    feat(editorFSNV3): V3SocialProof flottant sur M4 et M6

--- web/src/components/M4Scratch.tsx
+++ web/src/components/M4Scratch.tsx
@@ -9,6 +9,7 @@ import * as React from "react";
 import { sfx } from "../lib/v3_sound";
 import { pseudoTextStyle, pseudoPillStyle, type V3LineStyleLike } from "../lib/v3_pseudo_style";
 import { V3OfferPopup } from "./V3OfferPopup";
+import { V3SocialProof } from "./V3SocialProof";
 
 export type M4ScratchProps = {
   pseudo?: string;
@@ -326,6 +327,8 @@ export function M4Scratch({ pseudo, profileImageUrl, depositAmount, bonusAmount,
         steps={popupSteps}
         href={safeAffi}
       />
+
+      <V3SocialProof bonusAmount={bon} accent={T.accent} accentGlow={`${T.accent}66`} />
     </div>
   );
 }
--- web/src/components/M6Chest.tsx
+++ web/src/components/M6Chest.tsx
@@ -8,6 +8,7 @@ import * as React from "react";
 import { sfx } from "../lib/v3_sound";
 import { pseudoTextStyle, pseudoPillStyle, type V3LineStyleLike } from "../lib/v3_pseudo_style";
 import { V3OfferPopup } from "./V3OfferPopup";
+import { V3SocialProof } from "./V3SocialProof";
 
 export type M6ChestProps = {
   pseudo?: string;
@@ -288,6 +289,8 @@ export function M6Chest({ pseudo, profileImageUrl, depositAmount, bonusAmount, a
         steps={popupSteps}
         href={safeAffi}
       />
+
+      <V3SocialProof bonusAmount={bon} accent={T.accent} accentGlow={`${T.accent}66`} />
     </div>
   );
 }
